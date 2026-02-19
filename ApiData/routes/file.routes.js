09fb378d35ec7027ca98820c78d76419e01d0ffc
nikhil1042 <nikhilk1042003@gmail.com>
2026-02-19 15:24:08 +0530

    update

--- ApiData/routes/file.routes.js
+++ ApiData/routes/file.routes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
 import { verifyToken } from "../middleware/auth.middleware.js";
 import { checkRole } from "../middleware/role.middleware.js";
 import {
@@ -8,10 +10,15 @@ import {
   deleteFile
 } from "../controllers/file.controller.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const router = express.Router();
 
 const storage = multer.diskStorage({
-  destination: "uploads/",
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../uploads/"));
+  },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
   }
